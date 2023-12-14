061cdacdd9c4434dd238e10da251ac0747427fe4
paulohfsantos <paulohfsantos1@gmail.com>
2023-12-14 00:35:54 -0300

    refactor: fix router file

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,7 +6,7 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '',
-      component: () => import('pages/IndexPage.vue')
+        component: () => import('pages/Index.vue')
       }
     ],
   },
