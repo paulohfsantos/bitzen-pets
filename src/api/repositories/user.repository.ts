9aae6928406b5691662a31167b5f6295cb58a30a
paulohfsantos <paulohfsantos1@gmail.com>
2023-12-16 22:44:04 -0300

    refactor: user repository, service

--- src/api/repositories/user.repository.ts
+++ src/api/repositories/user.repository.ts
@@ -1,11 +1,12 @@
+import { AxiosResponse } from "axios";
 import { UserEditDto } from "../models/dtos/edituser.dto";
-import { UserRegisterDto } from "../models/dtos/register.dto";
 import { User } from "../models/entities/user.model";
 
 export interface UserRepository {
-  createUser(user: UserRegisterDto): Promise<User>;
-  getUserById(id: number): Promise<User>;
-  getUsers(): Promise<User[]>;
-  updateUserById(id: number, user: UserEditDto): Promise<User>;
-  deleteUserById(id: number): Promise<void>;
+  getLoggedUser(): Promise<AxiosResponse<User>>;
+  getUserInfo(): string | null;
+  updateUser(user: UserEditDto): Promise<AxiosResponse<User>>;
+  storeUserInfo(userInfo: string): void;
+  clearUserInfo(): void;
+  isUserLoggedIn(): boolean;
 }
--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,9 +1,13 @@
+import { AxiosResponse } from "axios";
 import { api } from "..";
+import { User } from "../models/entities/user.model";
+import { UserRepository } from "../repositories/user.repository";
+import { UserEditDto } from "../models/dtos/edituser.dto";
 
-export class UserService {
+export class UserService implements UserRepository {
   private readonly USER_KEY = 'user_info';
 
-  async getLoggedUser() {
+  async getLoggedUser(): Promise<AxiosResponse<User>> {
     try {
       const response = await api.get('/api/user');
 
@@ -13,8 +17,18 @@ export class UserService {
     }
   }
 
+  async updateUser(body: UserEditDto): Promise<AxiosResponse<User>> {
+    try {
+      const response = await api.put('/api/user/', body);
+
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   getUserInfo() {
-    return localStorage.getItem(this.USER_KEY);
+    return localStorage.getItem(this.USER_KEY)
   }
 
   storeUserInfo(userInfo: string) {
