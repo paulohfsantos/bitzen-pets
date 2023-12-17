de30790107a332ec03074237f16d3ff210963c8a
paulohfsantos <paulohfsantos1@gmail.com>
2023-12-16 22:46:32 -0300

    add: user store

---
+++ src/stores/user.store.ts
@@ -0,0 +1,43 @@
+import { defineStore } from "pinia";
+import { UserEditDto } from "src/api/models/dtos/edituser.dto";
+import { User } from "src/api/models/entities/user.model";
+import { UserService } from "src/api/services/user.service";
+import { computed, ref } from "vue";
+
+export const useUser = defineStore('user', () => {
+  const userService = new UserService();
+  const user = ref<User>();
+  const isAuthenticated = computed(() => !!user.value);
+  const isUnauthenticated = computed(() => !user.value);
+
+  function setUser(newUser: User) {
+    user.value = newUser;
+  }
+
+  function getUser() {
+    return userService.getLoggedUser();
+  }
+
+  function getUserInfo() {
+    return userService.getUserInfo();
+  }
+
+  function updateUser(body: UserEditDto) {
+    return userService.updateUser(body);
+  }
+
+  return {
+    // state
+    user,
+    isAuthenticated,
+    isUnauthenticated,
+
+    // setters
+    setUser,
+
+    // actions
+    getUser,
+    getUserInfo,
+    updateUser
+  };
+});
